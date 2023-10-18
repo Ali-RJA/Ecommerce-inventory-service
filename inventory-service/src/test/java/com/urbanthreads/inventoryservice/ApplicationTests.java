0f491bcac0d31c8775f7ec16bff1a51edb5cc717
Ali Alabbasi <ali.alabbasi03@gmail.com>
2023-10-18 01:47:01 -0400

    First commit which includes the project structure

---
+++ inventory-service/src/test/java/com/urbanthreads/inventoryservice/ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.urbanthreads.inventoryservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
