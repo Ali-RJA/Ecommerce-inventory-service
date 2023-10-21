44367d27af264f7c8d9a920b9143ab3566576a9a
Ali Alabbasi <ali.alabbasi03@gmail.com>
2023-10-21 19:54:24 -0400

    Fixed dependency issues and confirmed connection to RDS

---
+++ inventory-service/src/main/java/com/urbanthreads/inventoryservice/model/DatabaseConnectionChecker.java
@@ -0,0 +1,26 @@
+package com.urbanthreads.inventoryservice.model;
+
+import jakarta.annotation.PostConstruct;
+import jakarta.persistence.*;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+
+@Component
+public class DatabaseConnectionChecker {
+
+    @PersistenceContext
+    private EntityManager entityManager;
+
+    @PostConstruct
+    public void init() {
+        try {
+            entityManager.createNativeQuery("SELECT 1").getSingleResult();
+            System.out.println("Successfully connected to the database!");
+        } catch(Exception e) {
+            System.out.println("Failed to connect to the database.");
+            e.printStackTrace();
+        }
+    }
+}
--- inventory-service/src/main/java/com/urbanthreads/inventoryservice/model/Item.java
+++ inventory-service/src/main/java/com/urbanthreads/inventoryservice/model/Item.java
@@ -1,22 +1,39 @@
 package com.urbanthreads.inventoryservice.model;
 
+import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.Setter;
+
 
 import java.math.BigDecimal;
 
 @Setter
 @Getter
 @AllArgsConstructor
+@Entity
+@Table(name= "Item")
 public class Item {
 
-    private Long itemId;
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+    @Column(name = "item_name", nullable = false)
     private String itemName;
+    @Column(name = "description", nullable = false)
     private String description;
+    @Column(name = "price", nullable = false)
     private BigDecimal price;
+    @Column(name = "stock_quantity", nullable = false)
     private int stockQuantity;
+    @Column(name = "category")
     private String category;
+    @Column(name = "image_url")
     private String imageUrl;
 
+
+    public Item() {
+
+    }
 }
